f93e406dee26230ebfe5be6386d32c1464bd0216
jijieh-ui <cruzkatelee@gmail.com>
2026-08-25 12:35:11 +0800

    Add files via upload

---
+++ LabEx02.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hexinzirkel;
+
+/**
+ *
+ * @author jaijai
+ */
+public class Hexinzirkel {
+
+    public static void main(String[] args) {
+        String CharacterName = "rheindotr";
+        String ClassDesc = "a spell caster, that can warp reality";
+        String Class = "mage";
+        String Race = "Dark elf";
+        String RaceDesc = "used to be a holy elf, but corrupted by the dark forest";
+        String Backstory = "banished by the church for getting caught using forbidden magic, tampered with reality, and now lives as a husk of what he used to be";
+        String Guild = "Hexinzerkel";
+
+
+        System.out.println("Welcome! "+CharacterName+", Welcome to baldur's gate! ");
+    }
+}
